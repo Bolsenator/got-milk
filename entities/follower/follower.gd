f0c12f3b5e98347e9c00e958a4d640b46e3c8ff9
Luke Bolser <luke.bolser@gmail.com>
2026-06-13 14:03:12 -0500

    changed follower collision to a softer repelling force

--- entities/follower/follower.gd
+++ entities/follower/follower.gd
@@ -4,6 +4,7 @@ extends CharacterBody2D
 @onready var navigation_agent = $NavigationAgent2D
 @onready var aggro_range = $AggroRange
 @onready var hitbox = $Hitbox
+@onready var follower_to_follower_hitbox = $FollowerToFollowerHitbox
 
 @onready var attack_sound = $AttackSound
 @onready var summon_sound = $SummonSound
@@ -15,6 +16,7 @@ var speed: float = 425.0
 var soft_leash_radius = 300.0
 var hard_leash_radius = 900.0
 var deceleration = 2.0
+var follower_to_follower_repulsion_speed = 25.0
 var attack_cushion: float = 16 # Prevents sprite from going crazy when right on enemy
 var attack_cooldown: float = 0.1
 var cooldown_timer: float = 0.0
@@ -37,17 +39,21 @@ func _physics_process(delta: float):
 	cooldown_timer -= delta
 	if player == null:
 		return
-		
+	
+	# Keep within hard leash distance
 	player_distance = global_position.distance_to(player.global_position)
 	if player_distance > hard_leash_radius:
 			target_enemy = null
 			state = State.FOLLOW
+	
+	# Find nearest enemy within range
 	if target_enemy == null:
 		var closest = get_closest_enemy()
 		if closest:
 			target_enemy = closest
 			target_enemy.tree_exited.connect(_on_target_died)
-		
+	
+	# Set targetting state
 	if target_enemy != null:
 		enemy_in_range = target_enemy in hitbox.get_overlapping_bodies()
 		target_enemy_distance = global_position.distance_to(target_enemy.global_position)
@@ -58,6 +64,7 @@ func _physics_process(delta: float):
 	else:
 		state = State.FOLLOW
 	
+	# Act on state
 	match state:
 		State.ATTACK:
 			# Attack if close
@@ -68,15 +75,21 @@ func _physics_process(delta: float):
 			# Chase if fard
 			if target_enemy_distance > attack_cushion:
 				navigation_agent.target_position = target_enemy.global_position
-				var dir = to_local(navigation_agent.get_next_path_position()).normalized()
-				velocity = dir * speed
+				var direction = to_local(navigation_agent.get_next_path_position()).normalized()
+				velocity = direction * speed
 		State.FOLLOW:
 			if player_distance > soft_leash_radius:
 				navigation_agent.target_position = player.global_position
-				var dir = to_local(navigation_agent.get_next_path_position()).normalized()
-				velocity = dir * speed
+				var direction = to_local(navigation_agent.get_next_path_position()).normalized()
+				velocity = direction * speed
 			else:
 				velocity = velocity.lerp(Vector2.ZERO, deceleration * delta)
+	
+	# Add follower to follower nudge to prevent grouping
+	for area in follower_to_follower_hitbox.get_overlapping_areas():
+		if area.is_in_group("follower_to_follower_hitbox"):
+			var direction = (global_position - area.global_position).normalized()
+			velocity += direction * follower_to_follower_repulsion_speed
 	
 	move_and_slide()
 	animated_sprite.flip_h = velocity.x < 0
