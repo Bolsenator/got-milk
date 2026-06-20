93af8f659f452b9a02486bf4c1aa136883cf91fb
Luke Bolser <luke.bolser@gmail.com>
2026-06-20 17:24:54 -0500

    renamed variable to remove name collision

--- entities/exp/exp.gd
+++ entities/exp/exp.gd
@@ -6,9 +6,9 @@ var exp_value: float = 0
 func _ready():
 	animated_sprite.play("default")
 
-func initialize(value: float = 0, position: Vector2 = Vector2(0,0)):
+func initialize(value: float = 0, spawn_position: Vector2 = Vector2(0,0)):
 	exp_value = value
-	global_position = position
+	global_position = spawn_position
 
 func _on_body_entered(body):
 	if body.is_in_group("player"):
