d3c2bb8409c58eb0e8fa2ef182e6c839e3e724df
Luke Bolser <luke.bolser@gmail.com>
2026-07-07 23:14:56 -0500

    refactored upgrade pool array to add elements from the inspector

---
+++ data/waves/WaveDefinition.gd
@@ -0,0 +1,8 @@
+class_name WaveDefinition
+extends Resource
+
+@export var time: float
+@export var enemy_scene: PackedScene
+@export var count: int
+@export var interval: float
+@export var boss_scene: PackedScene # optional, null if no boss this wave
