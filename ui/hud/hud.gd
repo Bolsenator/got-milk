76931e03cb6151d25abd100f2167d31557bedbcd
Luke Bolser <luke.bolser@gmail.com>
2026-07-25 23:45:32 -0500

    Collapsed duplicated upgrade widget scenes and scripts

--- ui/hud/hud.gd
+++ ui/hud/hud.gd
@@ -20,8 +20,8 @@ extends Control
 @export var multi_attack_scene: PackedScene
 
 var offscreen_indicator_scene: PackedScene = preload("res://entities/helpers/offscreen_indicator.tscn")
-
-var upgrade_scenes: Dictionary
+var upgrade_widget_scene: PackedScene = preload("uid://n2d0gi6ngryu")
+#var upgrade_scenes: Dictionary
 var active_upgrade_widgets: Dictionary
 
 func _ready() -> void:
@@ -32,20 +32,20 @@ func _ready() -> void:
 	exp_bar_ui.value = player.current_exp
 	player_level.text = "Lvl " + str(player.player_level)
 	
-	upgrade_scenes = {
-		UpgradeDefinition.Stat.SUMMON_MINION: summon_minion_scene,
-		UpgradeDefinition.Stat.MAX_HEALTH: max_health_scene,
-		UpgradeDefinition.Stat.HEALTH_REGEN: health_regen_per_sec_scene,
-		UpgradeDefinition.Stat.DAMAGE_REDUCTION: damage_reduction_scene,
-		UpgradeDefinition.Stat.PLAYER_MOVEMENT_SPEED: player_movement_speed_scene,
-		UpgradeDefinition.Stat.EXP_GAIN: exp_gain_scene,
-		UpgradeDefinition.Stat.DAMAGE: damage_scene,
-		UpgradeDefinition.Stat.ATTACK_COOLDOWN: attack_cooldown_scene,
-		UpgradeDefinition.Stat.MINION_MOVEMENT_SPEED: minion_movement_speed_scene,
-		UpgradeDefinition.Stat.CRIT_CHANCE: crit_chance_scene,
-		UpgradeDefinition.Stat.CRIT_DAMAGE: crit_damage_scene,
-		UpgradeDefinition.Stat.MULTI_ATTACK: multi_attack_scene
-	}
+	#upgrade_scenes = {
+		#UpgradeDefinition.Stat.SUMMON_MINION: summon_minion_scene,
+		#UpgradeDefinition.Stat.MAX_HEALTH: max_health_scene,
+		#UpgradeDefinition.Stat.HEALTH_REGEN: health_regen_per_sec_scene,
+		#UpgradeDefinition.Stat.DAMAGE_REDUCTION: damage_reduction_scene,
+		#UpgradeDefinition.Stat.PLAYER_MOVEMENT_SPEED: player_movement_speed_scene,
+		#UpgradeDefinition.Stat.EXP_GAIN: exp_gain_scene,
+		#UpgradeDefinition.Stat.DAMAGE: damage_scene,
+		#UpgradeDefinition.Stat.ATTACK_COOLDOWN: attack_cooldown_scene,
+		#UpgradeDefinition.Stat.MINION_MOVEMENT_SPEED: minion_movement_speed_scene,
+		#UpgradeDefinition.Stat.CRIT_CHANCE: crit_chance_scene,
+		#UpgradeDefinition.Stat.CRIT_DAMAGE: crit_damage_scene,
+		#UpgradeDefinition.Stat.MULTI_ATTACK: multi_attack_scene
+	#}
 
 func _on_exp_changed(new_exp: float, max_exp: float) -> void:
 	exp_bar_ui.max_value = max_exp
@@ -63,8 +63,9 @@ func update_time_elapsed(time_elapsed: float) -> void:
 func update_upgrades_display(upgrade: UpgradeDefinition, count: int) -> void:
 	var stat: UpgradeDefinition.Stat = upgrade.stat
 	if !active_upgrade_widgets.has(stat):
-		var new_widget: Control = upgrade_scenes[stat].instantiate()
+		var new_widget: Control = upgrade_widget_scene.instantiate()
 		upgrades_display.add_child(new_widget)
+		new_widget.texture_rect.texture = upgrade.icon
 		active_upgrade_widgets[stat] = new_widget
 	active_upgrade_widgets[stat].update_display(upgrade, count)
 
--- ui/hud/upgrade_widget.gd
+++ ui/hud/upgrade_widget.gd
@@ -1,5 +1,6 @@
 extends Control
 
+@onready var texture_rect: TextureRect = $TextureRect
 @onready var label: Label = $Label
 
 func update_display(upgrade: UpgradeDefinition, count: int) -> void:
